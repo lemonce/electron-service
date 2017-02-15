bfc94c9b3a44ed6f0d3cb81b2a330ecb70d738d6
Weilin <934587911@qq.com>
2017-02-15 19:00:22 +0800

    I told you early.

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,12 @@
 const {Logger, transports} = require('winston');
 const fs = require('fs');
 const fsExtra = require('fs-extra');
+const path = require('path');
+const {app} = require('electron');
+const logPath = path.join(app.getPath('appData'), '.config');
 
-fsExtra.ensureDirSync('logs');
-const DEFAULT_LOG = 'logs/application.log';
+fsExtra.ensureDirSync(logPath);
+const DEFAULT_LOG = path.join(logPath, 'application.log');
 fs.unlink(DEFAULT_LOG, err => err);
 // Init default log
 const {Console, File} = transports;
